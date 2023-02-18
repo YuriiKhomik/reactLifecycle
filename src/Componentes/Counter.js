44a0c13d6490d44e0fa85be43905b859fea735b9
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-02-18 20:44:38 +0200

    ...

---
+++ src/Componentes/Counter.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+class Counter extends React.Component {
+  static defaultProps = {
+    initialValue: 12,
+  };
+
+  static propTypes = {};
+
+  state = {
+    value: this.props.initialValue,
+  };
+  handleIncrement = () => {
+    this.setState((prevState) => {
+      return {
+        value: prevState.value + 1,
+      };
+    });
+  };
+
+  handleDecrement = () => {
+    this.setState((prevState) => ({ value: prevState.value - 1 }));
+  };
+
+  render() {
+    return (
+      <div className="Counter">
+        <span className="Counter__value">{this.state.value}</span>
+        <div className="Counter__controls">
+          <button type="button" onClick={this.handleIncrement}>
+            Increment on 1
+          </button>
+          <button type="button" onClick={this.handleDecrement}>
+            Decrement on 1
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
