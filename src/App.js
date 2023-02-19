c63e60ea3e7fa00d9e6fab6f30b7ba623c4a2a84
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-02-19 19:14:11 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // import Counter from "./Components/Counter/Counter";
-import Dropdown from "./Components/Dropdown/Dropdown";
+// import Dropdown from "./Components/Dropdown/Dropdown";
+import ColorPicker from "./Components/Colorpicker/Colorpicker";
+import colorPickerOptions from "./Components/Colorpicker/colorPickerOptions";
 
 export const App = () => {
   return (
     <>
       {/* <Counter initialValue={0} /> */}
-      <Dropdown />
+      {/* <Dropdown /> */}
+      <ColorPicker options={colorPickerOptions} />
     </>
   );
 };
