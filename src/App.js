9dd65d68fb8ef6589e240ae5216b8096c2bb8078
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-02-19 14:49:11 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Counter from "./Components/Counter/Counter";
+// import Counter from "./Components/Counter/Counter";
+import Dropdown from "./Components/Dropdown/Dropdown";
 
 export const App = () => {
   return (
     <>
-      <Counter initialValue={0} />
+      {/* <Counter initialValue={0} /> */}
+      <Dropdown />
     </>
   );
 };
--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -1,1 +1,45 @@
-console.log("asas");
+import React, { Component } from "react";
+import "./Dropdown.css";
+
+class Dropdown extends Component {
+  state = {
+    visible: false,
+  };
+
+  toggle = () => {
+    this.setState((prevState) => ({ visible: !prevState.visible }));
+  };
+
+  show = () => {
+    this.setState({ visible: true });
+  };
+
+  hide = () => {
+    this.setState({ visible: false });
+  };
+
+  render() {
+    return (
+      <div className="Dropdown">
+        <button type="button" className="Dropdown__toggle" onClick={this.show}>
+          Show
+        </button>
+        <button type="button" className="Dropdown__toggle" onClick={this.hide}>
+          Hide
+        </button>
+        <button
+          type="button"
+          className="Dropdown__toggle"
+          onClick={this.toggle}
+        >
+          {this.state.visible ? "Hide" : "Show"}
+        </button>
+        {this.state.visible && (
+          <div className="Dropdown__menu">This is dropdown-menu</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Dropdown;
