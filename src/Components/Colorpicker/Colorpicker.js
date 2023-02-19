b2c2f8721f23a4f6b0e5a5b4625b750e1c5d0e29
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-02-19 22:09:42 +0200

    ...

--- src/Components/Colorpicker/Colorpicker.js
+++ src/Components/Colorpicker/Colorpicker.js
@@ -2,38 +2,16 @@ import React, { Component } from "react";
 import "./ColorPicker.css";
 
 class ColorPicker extends Component {
-  state = {
-    activeOptionIdx: 1,
-  };
-
-  makeOptionClassName = (index) => {
-    const optionClasses = ["ColorPicker__option"];
-
-    if (index === this.state.activeOptionIdx) {
-      optionClasses.push("ColorPicker__option--active");
-    }
-
-    return optionClasses.join(" ");
-  };
-
-  setActiveIdx = (index) => {
-    this.setState({ activeOptionIdx: index });
-  };
-
   render() {
     return (
       <div className="ColorPicker">
         <h2 className="ColorPicker__title">Color Picker</h2>
-        <p>chosen color: </p>
         <div>
-          {this.props.options.map(({ label, color }, index) => (
+          {this.props.options.map(({ label, color }) => (
             <button
               key={label}
-              className={this.makeOptionClassName(index)}
-              style={{
-                backgroundColor: color,
-              }}
-              onClick={() => this.setActiveIdx(index)}
+              className="ColorPicker__option"
+              style={{ backgroundColor: color }}
             ></button>
           ))}
         </div>
@@ -41,6 +19,48 @@ class ColorPicker extends Component {
     );
   }
 }
+
+// class ColorPicker extends Component {
+//   state = {
+//     activeOptionIdx: 1,
+//   };
+
+//   makeOptionClassName = (index) => {
+//     const optionClasses = ["ColorPicker__option"];
+
+//     if (index === this.state.activeOptionIdx) {
+//       optionClasses.push("ColorPicker__option--active");
+//     }
+
+//     return optionClasses.join(" ");
+//   };
+
+//   setActiveIdx = (index) => {
+//     this.setState({ activeOptionIdx: index });
+//   };
+
+//   render() {
+//     const { label } = this.props.options[this.state.activeOptionIdx];
+//     return (
+//       <div className="ColorPicker">
+//         <h2 className="ColorPicker__title">Color Picker</h2>
+//         <p>chosen color: {label}</p>
+//         <div>
+//           {this.props.options.map(({ label, color }, index) => (
+//             <button
+//               key={label}
+//               className={this.makeOptionClassName(index)}
+//               style={{
+//                 backgroundColor: color,
+//               }}
+//               onClick={() => this.setActiveIdx(index)}
+//             ></button>
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 // const ColorPicker = ({ options }) => (
 //   <div className="ColorPicker">
