af4e8ff9c4b07ce9e89b47ecf1cfbada8c36a7f5
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-02-20 19:11:17 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -10,21 +10,37 @@ export class App extends Component {
     todos: [
       { id: "id-1", text: "Todo-1", completed: false },
       { id: "id-2", text: "Todo-2", completed: false },
-      { id: "id-3", text: "Todo-3", completed: false },
+      { id: "id-3", text: "Todo-3", completed: true },
       { id: "id-4", text: "Todo-4", completed: false },
     ],
   };
 
-  deleteTodo(todoId) {}
+  deleteTodo = (todoId) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((todo) => todo.id !== todoId),
+    }));
+  };
 
   render() {
     const { todos } = this.state;
+
+    // const completedTodos = todos.filter((todo) => todo.completed);
+    const completedTodos = todos.reduce(
+      (acc, todo) => (todo.completed ? acc + 1 : acc),
+      0
+    );
     return (
       <>
         {/* <Counter initialValue={0} />
         <Dropdown />
         <ColorPicker options={colorPickerOptions} /> */}
-        <TodoList todos={todos} />
+
+        <div>
+          <p>Total number of todo: {todos.length}</p>
+          <p>Total of completed: {completedTodos}</p>
+        </div>
+
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
       </>
     );
   }
--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,12 +1,13 @@
 import "./TodoList.css";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDeleteTodo }) => {
   return (
     <ul className="TodoList">
       {todos.map(({ id, text, completed }) => {
         return (
           <li key={id} className="TodoList__item">
-            <p className="TodoList__text">{text}</p> <button>delete</button>
+            <p className="TodoList__text">{text}</p>{" "}
+            <button onClick={() => onDeleteTodo(id)}>delete</button>
           </li>
         );
       })}
