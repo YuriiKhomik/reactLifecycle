da7a0d36f37909df6e52094df99bea95af0f680e
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-03-01 15:55:34 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -4,19 +4,43 @@ import { Component } from "react";
 // import Dropdown from "./Components/Dropdown";
 // import ColorPicker from "./Components/Colorpicker";
 // import colorPickerOptions from "./Components/Colorpicker/colorPickerOptions";
-// import TodoEditor from "./Components/TodoEditor/TodoEditor";
-// import TodoList from "./Components/TodoList";
-import initialTodos from "./Components/TodoList/todos.json";
+import TodoEditor from "./Components/TodoEditor/TodoEditor";
+import TodoList from "./Components/TodoList";
+// import initialTodos from "./Components/TodoList/todos.json";
 // import Form from "./Components/Form";
-// import Filter from "./Components/Filter";
+import Filter from "./Components/Filter";
+import Modal from "./Components/Modal";
 // import LoginForm from "./Components/LoginForm";
 // import ProductReviewForm from "./Components/ProductReviewForm";
 
 class App extends Component {
   state = {
-    todos: initialTodos,
+    // todos: initialTodos,
+    todos: [],
     filter: "",
+    showModal: false,
   };
+
+  componentDidMount() {
+    // console.log("App componentDidMount");
+    const todos = localStorage.getItem("todos");
+    const parsedTodos = JSON.parse(todos);
+
+    if (parsedTodos) {
+      this.setState({
+        todos: parsedTodos,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log("App componentDidUpdate");
+    // порівнюємо не масиви, а їх властивості(тому, що якщо будемо порівнювати навіть
+    // два ідентичних масиви - вони будуть різні, бо мають різні посилання)
+    if (this.state.todos !== prevState.todos) {
+      localStorage.setItem("todos", JSON.stringify(this.state.todos));
+    }
+  }
 
   addTodo = (message) => {
     const todo = {
@@ -35,7 +59,7 @@ class App extends Component {
   };
 
   toggleCompleted = (todoId) => {
-    console.log(todoId);
+    // console.log(todoId);
     // this.setState((prevState) => ({
     //   todos: prevState.todos.map((todo) => {
     //     if (todo.id === todoId) {
@@ -77,129 +101,61 @@ class App extends Component {
     return todos.reduce((acc, todo) => (todo.completed ? acc + 1 : acc), 0);
   };
 
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
   render() {
-    // const { todos } = this.state;
-    // const completedTodos = this.getCompletedTodoCount();
-    // const totatTodosCount = todos.length;
-    // const visibleTodos = this.getVisibleTodos();
+    // console.log("App render");
+    const { todos, filter, showModal } = this.state;
+    const completedTodos = this.getCompletedTodoCount();
+    const totatTodosCount = todos.length;
+    const visibleTodos = this.getVisibleTodos();
 
     return (
+      // todos:
       <>
-        {/* <div>
+        <div>
           <p>Total number of todo: {totatTodosCount}</p>
           <p>Total of completed: {completedTodos}</p>
         </div>
         <TodoEditor onSubmit={this.addTodo} />
-
-        <Filter onChange={this.changeFilter} value={this.state.filter} />
-
+        <Filter onChange={this.changeFilter} value={filter} />
         <TodoList
           todos={visibleTodos}
           onDeleteTodo={this.deleteTodo}
-          onToggleCompleted={this.toggleCompleted} */}
-        {/* <LoginForm /> */}
-        {/* <ProductReviewForm /> */}
+          onToggleCompleted={this.toggleCompleted}
+        />
+
+        <>
+          <button type="button" onClick={this.toggleModal}>
+            Open modal
+          </button>
+          {showModal && (
+            <Modal onClose={this.toggleModal}>
+              <h1>Hey, this is content on modal as children</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Officia, delectus facilis? Similique voluptatibus inventore
+                animi hic sint itaque eos alias? Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Officia, delectus facilis?
+                Similique voluptatibus inventore animi hic sint itaque eos
+                alias?
+              </p>
+              <button type="button" onClick={this.toggleModal}>
+                Close modal
+              </button>
+            </Modal>
+          )}
+        </>
       </>
+
+      // {/* <LoginForm /> */}
+      // {/* <ProductReviewForm /> */}
     );
   }
 }
 
 export default App;
-
-// class App extends Component {
-// state = {
-//   todos: initialTodos,
-// };
-
-//   // formSubmitHandler = (submitData) => {
-//   //   console.log(submitData);
-//   // };
-// deleteTodo = (todoId) => {
-//   this.setState((prevState) => ({
-//     todos: prevState.todos.filter((todo) => todo.id !== todoId),
-//   }));
-// };
-
-//   render() {
-//     // return (
-//     //   <>
-//     //     <Form onSubmit={this.formSubmitHandler} />
-//     //   </>
-//     // );
-//     // return (
-//     //   <>
-//     //     <ColorPicker options={colorPickerOptions} />
-//     //   </>
-//     // );
-//     const { todos } = this.state;
-//     // const completedTodos = todos.filter((todo) => todo.completed);
-//     const totatTodosCount = todos.length;
-//     const completedTodosCount = todos.reduce(
-//       (acc, todo) => (todo.completed ? acc + 1 : acc),
-//       0
-//     );
-//     return (
-//       <>
-//         <div>
-//           <p>Total number of todo: {totatTodosCount}</p>
-//           <p>Total of completed: {completedTodosCount}</p>
-//         </div>
-//         <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
-//       </>
-//     );
-//   }
-// }
-
-// export default App;
-
-// export class App extends Component {
-// state = {
-//   todos: [
-//     { id: "id-1", text: "Todo-1", completed: false },
-//     { id: "id-2", text: "Todo-2", completed: false },
-//     { id: "id-3", text: "Todo-3", completed: true },
-//     { id: "id-4", text: "Todo-4", completed: false },
-//   ],
-// };
-
-//   deleteTodo = (todoId) => {
-//     this.setState((prevState) => ({
-//       todos: prevState.todos.filter((todo) => todo.id !== todoId),
-//     }));
-//   };
-
-//   render() {
-//     const { todos } = this.state;
-
-//     // const completedTodos = todos.filter((todo) => todo.completed);
-//     const completedTodos = todos.reduce(
-//       (acc, todo) => (todo.completed ? acc + 1 : acc),
-//       0
-//     );
-//     return (
-//       <>
-//         {/* <Counter initialValue={0} />
-//         <Dropdown />
-//         <ColorPicker options={colorPickerOptions} /> */}
-
-// <div>
-//   <p>Total number of todo: {todos.length}</p>
-//   <p>Total of completed: {completedTodos}</p>
-// </div>
-
-//         <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
-//       </>
-//     );
-//   }
-// }
-
-// export const App = () => {
-// return (
-//   <>
-//     {/* <Counter initialValue={0} />
-// <Dropdown />
-// <ColorPicker options={colorPickerOptions} /> */}
-//     <TodoList />
-//   </>
-// );
-// };
---
+++ src/Components/Modal/Modal.js
@@ -0,0 +1,47 @@
+import { Component } from "react";
+import { createPortal } from "react-dom";
+import "./Modal.css";
+
+const modalRoot = document.querySelector("#modal-root");
+
+class Modal extends Component {
+  componentDidMount() {
+    console.log("Modal componentDidMount");
+    window.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    console.log("Modal componentWillUnmount");
+    window.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  handleKeyDown = (e) => {
+    if (e.code === "Escape") {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = (e) => {
+    // console.log("click on backdrop");
+    // console.log("currentTarget", e.currentTarget);
+    // console.log("target", e.target);
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div className="Modal__backdrop" onClick={this.handleBackdropClick}>
+        <div className="Modal__content">{this.props.children}</div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+
+//   <div className="Modal__backdrop">
+//     <div className="Modal__content">{this.props.children}</div>
+//   </div>
+
+export default Modal;
